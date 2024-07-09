1d69ef9064192441150edf9525f92bc827aa9e22
shdiya29 <diyasharma292004@gmail.com>
2024-07-09 00:53:28 -0700

    first commit

---
+++ Gofood-main/backend/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', true);
+const mongoURI = 'mongodb+srv://food_delivery01:mern123@cluster0.a2zyylq.mongodb.net/foodeliveryapp?retryWrites=true&w=majority&appName=Cluster0'
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("foodCategory");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            // listCollections({name: 'food_items'}).toArray(function (err, database) {
+            // });
+            //     module.exports.Collection = database;
+            // });
+        }
+    })
+};
